b2e09b9cc8f2a4ee5cf41d228f41c6cf0fd121a5
andynze1 <andynze4@gmail.com>
2024-06-26 23:22:04 -0500

    remove vtech

--- webapp/src/main/java/com/vtech/account/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vtech.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vtech.account.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
--- webapp/src/main/java/com/vtech/account/service/ProducerService.java
+++
@@ -1,6 +0,0 @@
-package com.vtech.account.service;
-
-public interface ProducerService {
-
-    public String  produceMessage(String message);
-}
